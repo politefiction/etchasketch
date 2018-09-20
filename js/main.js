85a1b36cbc5104a2b4fc77413753444a9e8fd12c
Chelsi Jones <cajones.bz@gmail.com>
2018-09-20 06:53:41 -0500

    Update JS to ES6

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
-$(document).ready(function() {
-	var origWidth = 560;
-	var origHeight = 560;
-	$(".container").css({"width": origWidth + "px", "height": origHeight + "px"})
+window.onload = function() {
+	let origWidth = 560;
+	let origHeight = 560;
+	let container = document.querySelector(".container")
+	container.setAttribute('style', `width: ${origWidth}px; height: ${origHeight}px`);
 
 //Starting grid
-	var test3 = "Can you see me? ";
-	var defaultGrid = (origWidth * origHeight)/Math.pow(35, 2);
+	let test3 = "Can you see me? ";
+	let defaultGrid = (origWidth * origHeight)/Math.pow(35, 2);
 	function makeGrid(gridSize) {
-		for(var i=0; i < gridSize; i++) {
-			var square = $('<div><li></li></div>').css({"width":"35px", "height":"35px", "background-color": "white", "display": "inline-block"});
-			$(".container").append(square);
-			$(square).on("mouseenter", function() {
-				$(this).css("background-color", "black");
+		for(i=0; i < gridSize; i++) {
+			let square = document.createElement('div');
+			square.setAttribute('class', 'square');
+			square.appendChild(document.createElement('li'));
+			container.appendChild(square);
+			square.addEventListener("mouseenter", function() {
+				this.style.backgroundColor = "black";
 			});
 		};
 	};
@@ -19,25 +22,33 @@ $(document).ready(function() {
 	makeGrid(defaultGrid);
 
 // Make a new grid
-	$("#new-grid").click(function() {
-		var gridWidth = prompt("Grid width:");
-		var gridHeight = prompt("Grid height:");
+	let newGridStart = document.querySelector('#new-grid');
+	newGridStart.onclick = function() {
+		let gridWidth = prompt("Grid width:");
+		let gridHeight = prompt("Grid height:");
 		if (isNaN(gridWidth) === true || isNaN(gridHeight) === true) {
 			alert("Grid width and height must be numbers! Please try again.")
 		} else {
-			var test1 = gridWidth * 35;
-			var test2 = gridHeight * 35;
-			var newGrid = (test1 * test2)/Math.pow(35, 2);
-			$(".container").css({"width": test1 + "px", "height": test2 + "px"}).empty(".container");
+			let test1 = gridWidth * 35;
+			let test2 = gridHeight * 35;
+			let newGrid = (test1 * test2)/Math.pow(35, 2);
+			container.querySelectorAll('div').forEach(div => 
+				container.removeChild(div)
+			);
+			container.setAttribute('style', `width: ${test1}px; height: ${test2}px`);
 			makeGrid(newGrid);
 			alert("Current Grid Area: " + newGrid);
 		}; 
-	});
+	};
 
 
 // Clear grid
-	$("#shake").click(function() {
-		$(".container").find("*").css("background-color", "white");
-	})
+	let shake = document.querySelector('#shake');
+	shake.onclick = () => {
+		container.querySelectorAll('div').forEach(div => 
+			div.style.backgroundColor = "white" 
+		);
+	}
 
-});
+
+};
